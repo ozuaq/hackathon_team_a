08262b93430af970173da8f0cc089f444e77186c
ozuaq <oza4456@gmail.com>
2022-12-09 09:45:24 +0900

    update freedom_vue.js & freedom.html

--- static/js/freedom_vue.js
+++ static/js/freedom_vue.js
@@ -1,9 +1,12 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.7.11/dist/vue.esm.browser.js'
 
-var app = new Vue({
-  delimiters: ['[[', ']]'],
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
 })
+
+new Vue({ el: '#components-demo' })
